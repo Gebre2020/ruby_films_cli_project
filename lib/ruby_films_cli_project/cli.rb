76647258382df52e854c945008833e66ae73f79f
Gebre2020 <gebre20122@gmail.com>
2021-01-08 23:58:44 -0600

    creating classes and method

--- lib/ruby_films_cli_project/cli.rb
+++ lib/ruby_films_cli_project/cli.rb
@@ -1,6 +1,37 @@
 class CLI
 
   def start
-    puts "Welcome films selection"
+    puts "Welcome to the film Lot, shop for a new film?"
+    puts "Let's start with your name, please enter your name:"
+    API.get_data
+    # binding.pry
+    greet(user_input)
   end
+
+  def user_input
+    gets.strip
+  end
+
+  def greet(name)
+    puts "Awesome #{name}, would you like to see our films?"
+    puts "Enter the input 'y' to see the list, 'exit' to leave the lot!"
+    menu
+  end
+
+  # based on eser selection, either show a list of films,
+  # give them an error message, or exit
+
+  def menu
+    selection = user_input
+    if selection == 'y'
+      films_list
+      menu 
+    elsif selection == 'exit'
+      goodbye
+    else 
+      invalid
+    end
+  end
+
+
 end
--- lib/ruby_films_cli_project/film.rb
+++ lib/ruby_films_cli_project/film.rb
@@ -1,4 +1,22 @@
 class Film
 
+  @@all = []
+
+  attr_accessor :title, :director, :producer
+
+  def initialize(title, director, producer)
+    @title = title
+    @director = director
+    @producer = producer
+    save
+    end 
+
+  def save
+    @@all << self
+  end 
+    
+  def self.all
+    @@all
+  end
 
 end
